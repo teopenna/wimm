234fbab1bcfbc367227f1cd89ebdc1d2159b878b
Matteo Penna <penna.matteo@gmail.com>
2021-06-09 08:26:02 +0200

    Added Dto Validation Filters

---
+++ src/web-api/Wimm.API/DtoValidation/Filters/DtoFailedValidationResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Wimm.API.DtoValidation.Filters
+{
+    public class DtoFailedValidationResult
+    {
+        public int StatusCode { get; } = 400;
+        public IList<string> ModelValidationErrors { get; }
+
+        public DtoFailedValidationResult(IList<string> modelValidationErrors) => ModelValidationErrors = modelValidationErrors;
+    }
+}
---
+++ src/web-api/Wimm.API/DtoValidation/Filters/DtoValidationFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Wimm.API.DtoValidation.Filters
+{
+    public class DtoValidationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var modelValidationErrors = context.ModelState
+                        .Values
+                        .SelectMany(v => v.Errors.Select(b => b.ErrorMessage))
+                        .ToList();
+                var dtoFailedValidationResult = new DtoFailedValidationResult(modelValidationErrors);
+                context.Result = new JsonResult(dtoFailedValidationResult) { StatusCode = 400 };
+            }
+        }
+    }
+}
